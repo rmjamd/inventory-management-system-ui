25d155198d306686be65f24cdb43372310cb0ab7
Ramij <ramijnalpur@gmail.com>
2023-11-20 06:32:42 +0530

    Fixing issue

--- js/AddProduct.js
+++ js/AddProduct.js
@@ -1,6 +1,64 @@
 const apiEndpoint = 'http://localhost:4040/api/product-list';
 const designNamesEndpoint = 'http://localhost:4040/api/design/names';
 
+async function addProduct() {
+    const dn = document.getElementById("designName").value;
+    const quantity = parseInt(document.getElementById("quantity").value);
+    const currentCost = parseFloat(document.getElementById("currentCost").value);
+    const sizeName = document.getElementById("sizeName").value;
+    const color = document.getElementById("color").value;
+
+    if (!designName || !quantity || !currentCost || !sizeName || !color) {
+        alert("All fields are required!");
+        return;
+    }
+
+    if (isNaN(quantity) || quantity < 1) {
+        alert("Quantity must be a positive integer!");
+        return;
+    }
+
+    if (isNaN(currentCost) || currentCost <= 0) {
+        alert("Cost must be a positive number!");
+        return;
+    }
+
+
+    const productData = {
+        quantity: quantity,
+        cost: currentCost,
+        designName: dn,
+        size: {
+            sizeName: sizeName,
+        },
+        color: color,
+    };
+    try {
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(productData),
+        });
+
+        if (response.status >= 200 && response.statusn<300) {
+            alert("Product created successfully!");
+            document.getElementById("designName").value = "";
+            document.getElementById("quantity").value = "";
+            document.getElementById("currentCost").value = "";
+            document.getElementById("sizeName").value = "";
+        } else {
+            alert("Failed to create the product. Please try again.");
+        }
+    } catch (error) {
+        alert("An error occurred while sending the request.");
+        console.error(error);
+    }
+
+}
+
+
 // Function to fetch design names
 async function fetchDesignNames() {
     try {
